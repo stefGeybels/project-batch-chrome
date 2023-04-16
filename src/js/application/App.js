db58482afe00c595405b46136de1baf6808d0a56
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-16 12:17:54 +0200

    Succesfull switch between Login and dashboard

--- src/js/application/App.js
+++ src/js/application/App.js
@@ -1,15 +1,34 @@
 import React from "react";
 import Dashboard from "../popup/dashboard/Dashboard";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { User } from "../authentication/User";
 import Login from "../popup/login/Login";
 
 export default function App() {
     const [user, setUser] = useState(new User());
-    
+
+    useEffect(() => {
+        chrome.storage.local.get(['user'], function(data) {
+            if (data.user) {
+                const newUser = new User();
+                newUser.addToken(data.user);
+                setUser(newUser);
+            }
+        });
+    }, []);
+
     if (user.isAuthenticated()) {
         return <Dashboard />
     }
 
-    return <Login />
+    const loginAttempt = (userData) => {
+        const newUser = new User();
+        newUser.addToken("sdkjfhsk");
+        newUser.addEmail(userData.email);
+        newUser.addCompany(userData.company);
+        setUser(newUser);
+        chrome.storage.local.set({ user: newUser.getUser() });
+    }
+
+    return <Login loginAttempt={loginAttempt}/>
 }
