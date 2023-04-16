94b4e8cc4c7dd3598892b5acdc28ab9c99d5be89
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-16 12:36:55 +0200

    Show basic user info in plugin

--- src/js/popup/dashboard/Dashboard.js
+++ src/js/popup/dashboard/Dashboard.js
@@ -1,10 +1,20 @@
 import React from "react";
 import AuthenticatedUser from "../../authentication/User";
 
-export default function Dashboard() {
+export default function Dashboard({ userData }) {
+
+    function clearStorage() {
+        chrome.storage.local.remove(['user'], function() {
+            window.location.reload();
+        });
+    }
+
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>Email: {userData.getUser().email}</h1>
+            <h1>Company: {userData.getUser().company}</h1>
+            <h1>Token: {userData.getUser().token}</h1>
+            <button onClick={() => clearStorage()}>Button</button>
         </div>
     )
 }
