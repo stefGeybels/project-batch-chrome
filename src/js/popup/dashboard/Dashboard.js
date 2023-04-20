c6bdd968a9aaa19bea4c8981eeafd66999eaae86
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-20 20:25:12 +0200

    Update Dashboard layout

--- src/js/popup/dashboard/Dashboard.js
+++ src/js/popup/dashboard/Dashboard.js
@@ -24,20 +24,23 @@ export default function Dashboard({ userData }) {
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
                 <div className="space-y-6">
-                    <div>
-                        <p className="mt-2 text-base leading-7 text-gray-600">We are currently collecting data for {userData.getUser().email}</p>
-                        <p className="mt-2 text-base leading-7 text-gray-600">For the company {userData.getUser().company}</p>
-                    </div>
-
-                    <div>
-                        <button
-                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        <div className="sm:flex sm:space-x-5">
+                            <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                                <p className="text-sm font-medium text-gray-600">We are currently collecting data for,</p>
+                                <p className="text-xl font-bold text-gray-900 sm:text-2xl">{userData.getUser().email}</p>
+                                <p className="text-sm font-medium text-gray-600">{userData.getUser().company}</p>
+                            </div>
+                        </div>
+                        <div className="mt-5 flex justify-center sm:mt-0">
+                            <button
+                            href="#"
+                            className="flex items-center justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                             onClick={() => clearStorage()}
-                        >
+                            >
                             Logout
-                        </button>
+                            </button>
+                        </div>
                     </div>
-                </div>
 
                 </div>
             </div>
