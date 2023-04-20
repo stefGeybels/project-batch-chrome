a829529f192aa140cd8dc35a68d92b2ec3f165d2
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-20 20:18:32 +0200

    Update dashboard layout

--- src/js/popup/dashboard/Dashboard.js
+++ src/js/popup/dashboard/Dashboard.js
@@ -10,11 +10,38 @@ export default function Dashboard({ userData }) {
     }
 
     return (
-        <div>
-            <h1>Email: {userData.getUser().email}</h1>
-            <h1>Company: {userData.getUser().company}</h1>
-            <h1>Token: {userData.getUser().token}</h1>
-            <button onClick={() => clearStorage()}>Button</button>
+        <>
+        <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+                {/* <img
+                className="mx-auto h-12 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+                /> */}
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Welcome</h2>
+            </div>
+                
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+                <div className="space-y-6">
+                    <div>
+                        <p className="mt-2 text-base leading-7 text-gray-600">We are currently collecting data for {userData.getUser().email}</p>
+                        <p className="mt-2 text-base leading-7 text-gray-600">For the company {userData.getUser().company}</p>
+                    </div>
+
+                    <div>
+                        <button
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            onClick={() => clearStorage()}
+                        >
+                            Logout
+                        </button>
+                    </div>
+                </div>
+
+                </div>
+            </div>
         </div>
+        </>
     )
 }
