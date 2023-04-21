2f8a6df42104d8d04fc3013bc52689c5ec2bf626
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-21 18:21:51 +0200

    Adding safety into url parse functions

--- src/js/helpers/parseUrl.js
+++ src/js/helpers/parseUrl.js
@@ -1,10 +1,18 @@
 export class urlParser{
     findBaseUrl(url){
+        if(url == null){
+            return ''
+        }
+
         let pathArray = url.split('/')
         return pathArray[0] + '//' + pathArray[2]
     }
 
     findDomain(url){
+        if(url == null){
+            return ''
+        }
+        
         let pathArray = url.split('/')
         let domainArray = pathArray[2].split('.')
         return domainArray[1];
