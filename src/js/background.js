c313f783e0e60f767835a9e844d0fbc42b4d9811
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-24 00:50:23 +0200

    Monitor time on page for specific urls (#8)
    * Adding monitor to background js
    
    * Update variables.json
    
    * adding webNavigation permissions
    
    * Update background.js
    
    * Addin tab persmissions
    
    * Creating an object to parse web urls
    
    * Creating object to send API request for page visit
    
    * Adding safety into url parse functions
    
    * Send a new request when user switches monitord sites
    
    * Return correct number of settings
    
    * Adding onComplete background check
    
    * timing and sending data
    
    * Fixing not firing bug
    
    * Removing duplicate code
    
    * Remove getUrl() and un used variables
    
    * Update manifest.json
    
    * Remove unused function
    
    * Remove useless if loop

--- src/js/background.js
+++ src/js/background.js
@@ -1,15 +1,55 @@
-import React, { useEffect } from 'react'
-import { useDeferredValue } from 'react';
-import '../img/icon-128.png'
-import '../img/icon-34.png'
+import data from './variables.json'
+import { urlParser } from './helpers/parseUrl';
+import axios from 'axios';
+import { PageVisit } from './helpers/pageVisited';
 
-export default function background() {
-    chrome.runtime.onInstalled.addListener(() => {
-        chrome.contextMenus.create({
-          "id": "sampleContextMenu",
-          "title": "Sample Context Menu",
-          "contexts": ["selection"]
-        });
-      });
-  return null;
-}
+const urlsToTrack = [
+  "https://www.google.be",
+  "https://www.facebook.com"
+];
+
+// Define a variable to store the start time of the user's visit
+let startTime = null;
+let parser = new urlParser();
+let request = new PageVisit();
+
+console.log('startup background worker');
+
+let monitorUrl = null;
+let currentUrl = null;
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    currentUrl = parser.findBaseUrl(tabs[0].url)
+
+    if(urlsToTrack.includes(currentUrl)){
+      if(startTime == null){
+        startTime = new Date();
+        return;
+      }
+  
+      if(monitorUrl === null) 
+      {
+        monitorUrl = currentUrl
+        return;
+      }
+  
+      if(parser.findDomain(currentUrl) !== parser.findDomain(monitorUrl)){
+        const totalTime = Math.round((new Date() - startTime) / 1000);
+        console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+        // request.sendPageVisit(totalTime, monitorUrl)
+        startTime = new Date();
+      }
+      monitorUrl = currentUrl;
+      return;
+    }
+    
+    if(urlsToTrack.includes(monitorUrl) && monitorUrl !== null){
+      const totalTime = Math.round((new Date() - startTime) / 1000);
+      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+
+      monitorUrl = null;
+      startTime = null;
+    }
+  })
+});
