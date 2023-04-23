3833772efd4d0fbc57af9922e3b00c88b2fb657f
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-23 21:01:20 +0200

    timing and sending data

--- src/js/background.js
+++ src/js/background.js
@@ -17,96 +17,143 @@ console.log('startup background worker');
 
 let monitorUrl = null;
 let changeUrl = null;
-let newUrl = null;
+let currentUrl = null;
 
-// Listen for when the user navigates to a new page
-// Events get fired after user leaves the page
-chrome.webNavigation.onCommitted.addListener((details) => {
-  newUrl = parser.findBaseUrl(details.url)
+function getUrl(){
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    currentUrl = parser.findBaseUrl(tabs[0].url)
+    console.log(currentUrl)
+  })
+}
 
-  if(urlsToTrack.includes(newUrl)){
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  getUrl()
+
+  if(urlsToTrack.includes(currentUrl)){
     if(startTime == null){
       startTime = new Date();
+      return;
     }
 
-    console.log('before if : ' + newUrl, 'monitor url : ' + monitorUrl)
+    if(monitorUrl === null) 
+    {
+      monitorUrl = currentUrl
+      return;
+    }
 
-    if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl) && monitorUrl != null){
+    if(parser.findDomain(currentUrl) !== parser.findDomain(monitorUrl)){
       const totalTime = Math.round((new Date() - startTime) / 1000);
-      console.log('time spend on site: ' + totalTime)
+      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
       // request.sendPageVisit(totalTime, monitorUrl)
       startTime = new Date();
     }
-    monitorUrl = newUrl;
+    monitorUrl = currentUrl;
+
+    if(!urlsToTrack.includes(currentUrl))
+    {
+      startTime = null;
+    }
     // console.log('tracked url')
     return;
   }
-  // console.log('not a tracked url: ' + newUrl)
+  
+  if(urlsToTrack.includes(monitorUrl) && monitorUrl !== null){
+    const totalTime = Math.round((new Date() - startTime) / 1000);
+    console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+    // request.sendPageVisit(totalTime, monitorUrl)
+    monitorUrl = null;
+    startTime = null;
+  }
 });
 
-// Listen for when the user navigates away from a tracked page
-// This should listen to when a user navigates to a non tracked page
-// Event get fired when user enters a page
-chrome.webNavigation.onCompleted.addListener((details) => {
-  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-    changeUrl = parser.findBaseUrl(tabs[0].url)
-  })
+// // Listen for when the user navigates to a new page
+// // Events get fired after user leaves the page
+// chrome.webNavigation.onCommitted.addListener((details) => {
+//   newUrl = parser.findBaseUrl(details.url)
 
-  //stops event
-  //early return if are a good way to make checks before editing url
-  if(parser.findDomain(changeUrl) === parser.findDomain(monitorUrl) || changeUrl === null){
-    console.log('enterd if')
-    return;
-  }
+//   if(urlsToTrack.includes(newUrl)){
+//     if(startTime == null){
+//       startTime = new Date();
+//     }
 
-  if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl) && monitorUrl != null){
-    console.log('check enterd! Monitor: ' + monitorUrl + ' - newUrl: ' + newUrl)
-    // Variable needs to be changed here => other wise we don't have anything to prevent event from duplicate firing
-    monitorUrl = newUrl;
-    return;
-  }
+//     console.log('before if : ' + newUrl, 'monitor url : ' + monitorUrl)
 
-  console.log(changeUrl + ' - ' + monitorUrl)
+//     if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl) && monitorUrl != null){
+//       const totalTime = Math.round((new Date() - startTime) / 1000);
+//       console.log('time spend on site: ' + totalTime)
+//       // request.sendPageVisit(totalTime, monitorUrl)
+//       startTime = new Date();
+//     }
+//     monitorUrl = newUrl;
+//     // console.log('tracked url')
+//     return;
+//   }
+//   // console.log('not a tracked url: ' + newUrl)
+// });
 
-  // console.log('Navigation activated: ' + changeUrl)
+// // Listen for when the user navigates away from a tracked page
+// // This should listen to when a user navigates to a non tracked page
+// // Event get fired when user enters a page
+// chrome.webNavigation.onCompleted.addListener((details) => {
+//   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+//     changeUrl = parser.findBaseUrl(tabs[0].url)
+//   })
 
-  if(!urlsToTrack.includes(changeUrl)){
-    console.log('url is not included in list')
-  }
+//   //stops event
+//   //early return if are a good way to make checks before editing url
+//   if(parser.findDomain(changeUrl) === parser.findDomain(monitorUrl) || changeUrl === null){
+//     console.log('enterd if')
+//     return;
+//   }
 
-  // if(urlsToTrack.includes(newUrl)){
-  //   console.log('before if: ' + newUrl, 'monitor url: ' + monitorUrl)
-  //   if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl)){
-  //     // const totalTime = Math.round((Date.now() - startTime) / 60000);
-  //     // request.sendPageVisit(totalTime, monitorUrl)
-  //     // startTime = Date.now();
-  //     console.log(monitorUrl)
-  //   }
-  //   monitorUrl = newUrl;
-  // }
+//   if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl) && monitorUrl != null){
+//     console.log('check enterd! Monitor: ' + monitorUrl + ' - newUrl: ' + newUrl)
+//     // Variable needs to be changed here => other wise we don't have anything to prevent event from duplicate firing
+//     monitorUrl = newUrl;
+//     return;
+//   }
+
+//   console.log(changeUrl + ' - ' + monitorUrl)
+
+//   // console.log('Navigation activated: ' + changeUrl)
+
+//   if(!urlsToTrack.includes(changeUrl)){
+//     console.log('url is not included in list')
+//   }
+
+//   // if(urlsToTrack.includes(newUrl)){
+//   //   console.log('before if: ' + newUrl, 'monitor url: ' + monitorUrl)
+//   //   if(parser.findDomain(newUrl) !== parser.findDomain(monitorUrl)){
+//   //     // const totalTime = Math.round((Date.now() - startTime) / 60000);
+//   //     // request.sendPageVisit(totalTime, monitorUrl)
+//   //     // startTime = Date.now();
+//   //     console.log(monitorUrl)
+//   //   }
+//   //   monitorUrl = newUrl;
+//   // }
 
 
-  // if (!urlsToTrack.includes(details.url) && monitorUrl !== null) {
-  //   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-  //     changeUrl = parser.findBaseUrl(tabs[0].url)
-  //   })
+//   // if (!urlsToTrack.includes(details.url) && monitorUrl !== null) {
+//   //   chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+//   //     changeUrl = parser.findBaseUrl(tabs[0].url)
+//   //   })
 
-  //   if(parser.findDomain(monitorUrl) !== parser.findDomain(changeUrl))
-  //   {
-  //       const totalTime = Math.round((Date.now() - startTime) / 60000);
+//   //   if(parser.findDomain(monitorUrl) !== parser.findDomain(changeUrl))
+//   //   {
+//   //       const totalTime = Math.round((Date.now() - startTime) / 60000);
 
-  //       // axios.post(data.url + '/test', {
-  //       //   totalTime,
-  //       //   pageUrl: details.url
-  //       // })
-  //       //   .then(response => {
-  //       //     console.log("API post request sent successfully.");
-  //       //   })
-  //       //   .catch(error => {
-  //       //     console.error("Error sending API post request:", error);
-  //       //   });
-  //       changeUrl = null;
-  //       monitorUrl = null;
-  //   }
-  // }
-});
+//   //       // axios.post(data.url + '/test', {
+//   //       //   totalTime,
+//   //       //   pageUrl: details.url
+//   //       // })
+//   //       //   .then(response => {
+//   //       //     console.log("API post request sent successfully.");
+//   //       //   })
+//   //       //   .catch(error => {
+//   //       //     console.error("Error sending API post request:", error);
+//   //       //   });
+//   //       changeUrl = null;
+//   //       monitorUrl = null;
+//   //   }
+//   // }
+// });
