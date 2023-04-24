6c887f28817a6d948e6f383dc52417579e1aed2b
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-24 12:35:23 +0200

    Stop and start timer on focus change (#10)
    * Add onFocusChanged event
    
    * Prevent API call on refocus
    
    * Clean up code
    
    * Update version number

--- src/js/background.js
+++ src/js/background.js
@@ -8,7 +8,6 @@ const urlsToTrack = [
   "https://www.facebook.com"
 ];
 
-// Define a variable to store the start time of the user's visit
 let startTime = null;
 let parser = new urlParser();
 let request = new PageVisit();
@@ -18,10 +17,17 @@ console.log('startup background worker');
 let monitorUrl = null;
 let currentUrl = null;
 let tabChangeUrl = null;
+let wasFocused = true;
 
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     currentUrl = parser.findBaseUrl(tabs[0].url)
+
+    if(!wasFocused){
+      wasFocused = true;
+      return;
+    }
+    wasFocused = true;
 
     if(urlsToTrack.includes(currentUrl)){
       if(startTime == null){
@@ -37,7 +43,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
   
       if(parser.findDomain(currentUrl) !== parser.findDomain(monitorUrl)){
         const totalTime = Math.round((new Date() - startTime) / 1000);
-        console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+        console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl + ' - new search')
         // request.sendPageVisit(totalTime, monitorUrl)
         startTime = new Date();
       }
@@ -47,7 +53,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     
     if(urlsToTrack.includes(monitorUrl) && monitorUrl !== null){
       const totalTime = Math.round((new Date() - startTime) / 1000);
-      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl + ' - new search (not in list)')
 
       monitorUrl = null;
       startTime = null;
@@ -59,18 +65,43 @@ chrome.tabs.onActivated.addListener(function(activeInfo) {
   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
     tabChangeUrl = parser.findBaseUrl(tabs[0].url)
 
-    if (urlsToTrack.includes(monitorUrl) && monitorUrl != null) {
+    if (urlsToTrack.includes(monitorUrl) && monitorUrl != null && wasFocused) {
       const totalTime = Math.round((new Date() - startTime) / 1000);
-      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl)
+      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl + ' - tab changed')
       // request.sendPageVisit(totalTime, monitorUrl)
       startTime = null;
       monitorUrl = null;
     }
-  
+    
+    
     if(urlsToTrack.includes(tabChangeUrl)){  
       startTime = new Date();
       monitorUrl = tabChangeUrl;
+    }
+    wasFocused = true;
+  });
+});
+
+chrome.windows.onFocusChanged.addListener((windowId) => {
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    wasFocused = false;
+    if(urlsToTrack.includes(monitorUrl) && monitorUrl !== null){
+      const totalTime = Math.round((new Date() - startTime) / 1000);
+      console.log('time spend on site: ' + totalTime + ' - website: ' + monitorUrl + ' - window focus changed')
+      // request.sendPageVisit(totalTime, monitorUrl)
+      startTime = null;
+      monitorUrl = null;
       return;
     }
+    return;
+  }
+
+  chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+    currentUrl = parser.findBaseUrl(tabs[0].url)
+
+    if(urlsToTrack.includes(currentUrl)){
+      startTime = new Date();
+      monitorUrl = currentUrl;
+    }   
   });
 });
