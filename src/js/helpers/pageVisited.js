04a4a045fdc9acde3b5bd903720333075ab5f0d7
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-04-25 13:57:06 +0200

    Updating request function to use fetch

--- src/js/helpers/pageVisited.js
+++ src/js/helpers/pageVisited.js
@@ -1,20 +1,49 @@
 import axios from "axios";
-import data from '../variables.json'
+import variableData from '../variables.json'
+import { User } from "../authentication/User"
+import { stringify } from "postcss";
 
 export class PageVisit{
-    sendPageVisit(
-        totalTime, 
-        domain
-    ) {
-    axios.post(data.url + '/test', {
-          totalTime,
-          pageUrl: domain
-        })
-          .then(response => {
-            console.log("API post request sent successfully.");
-          })
-          .catch(error => {
-            console.error("Error sending API post request:", error);
-          });
-    }
+  constructor() {
+    this.url = variableData.url + '/api/v1/page-visit';
+    chrome.storage.local.get(['user'], function(data) {
+      if (data.user) {
+          const newUser = new User();
+          newUser.setUserFromStorage(data.user); 
+          this.user = newUser;
+      }
+    })
+  }
+
+  sendPageVisit(
+      totalTime, 
+      domain
+  ) {
+    chrome.storage.local.get(['user'], async function(data) {
+      if (data.user) {
+          const newUser = new User();
+          newUser.setUserFromStorage(data.user); 
+
+          try {
+            const response = await fetch(variableData.url + "/api/v1/page-visit", {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Connection': 'keep-alive',
+                'Accept': '*/*',
+              },
+              body: JSON.stringify({
+                email: newUser.getUser().email,
+                token: newUser.getToken(),
+                duration: totalTime,
+                url: domain,
+              })
+            })
+            console.log(response)
+          } catch (error) {
+            console.log(error)
+          }
+      }
+    })
+  }
 }
