074b3aba103292aec7a7f83cc8484244a5be57ee
stefGeybels <92160407+stefGeybels@users.noreply.github.com>
2023-05-14 11:49:13 +0200

    Updating dashboard layout

--- src/js/popup/dashboard/Dashboard.js
+++ src/js/popup/dashboard/Dashboard.js
@@ -30,28 +30,36 @@ export default function Dashboard({ userData }) {
     return (
         <>
         <div className="flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-            <div className="sm:mx-auto sm:w-full sm:max-w-md">
+            <div className="sm:mx-auto text-center sm:w-full sm:max-w-md">
                 {/* <img
                 className="mx-auto h-12 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="Your Company"
                 /> */}
-                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Welcome</h2>
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Welcome {userData.getUser().email}</h2>
+                <p className="text-sm font-medium text-gray-600">{userData.getUser().company}</p>
             </div>
                 
             <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
                 <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
                 <div className="space-y-6">
                         <div className="sm:flex sm:space-x-5">
-                            <div className="mt-4 text-center sm:mt-0 sm:pt-1 sm:text-left">
-                                <p className="text-sm font-medium text-gray-600">We are currently collecting data for,</p>
-                                <p className="text-xl font-bold text-gray-900 sm:text-2xl">{userData.getUser().email}</p>
-                                <p className="text-sm font-medium text-gray-600">{userData.getUser().company}</p>
-                                {
-                                    platforms.map((item, i) => (
-                                        <p key={i}>{item.name}</p>
-                                    ))
-                                }
+                            <div className="mt-2 text-center sm:mt-0 sm:pt-1 sm:text-left">
+                                <p className="text-sm mb-2 font-bold text-gray-600">We are currently tracking these platforms,</p>
+                                
+
+                                <ul role="list" className="divide-y divide-gray-100">
+                                {platforms.map((platform) => (
+                                    <li key={platform.id} className="flex gap-x-4 py-5">
+                                    <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={platform.icon_url} alt="" />
+                                    <div className="min-w-0 text-left">
+                                        <p className="text-sm font-semibold leading-6 text-gray-900">{platform.name}</p>
+                                        <p className="mt-1 truncate text-xs leading-5 text-gray-500">{platform.base_url}</p>
+                                    </div>
+                                    </li>
+                                ))}
+                                </ul>
+                                
                             </div>
                         </div>
                         <div className="mt-5 flex justify-center sm:mt-0">
